5e9fb25211338be99598af0231fe33c12702ab79
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-22 13:26:32 +0900

    기본 데이터 출력

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-//유튜브 접근 API
+const frame = document.querySelector('section');
+
+//API 설정
 const api_key = 'AIzaSyBzu-DiHzM7s-z6s-Elyy24zf2bBrf6ckk';
 const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
-const num = 4;
+const num = 5;
 const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
 fetch(resultURL)
 	.then((data) => data.json())
 	.then((json) => {
 		console.log(json);
+		let tags = '';
+
+		json.items.map((data) => {
+			tags += `
+      <article>
+        <h2>${data.snippet.title}</h2>
+        <p>${data.snippet.description}</p>
+        <span>${data.snippet.publishedAt}</span>
+      </article>
+      `;
+		});
+		frame.innerHTML = tags;
 	});
