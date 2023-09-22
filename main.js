22f0125d1f2ecc7b0a94f37dd3e97489b80ad2ef
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-22 14:30:36 +0900

    특정 글자수 이상 자르기, 날짜 스플릿해서 날짜만 남기고 년월일 사이 대쉬 닷으로 수정

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const frame = document.querySelector('section');
+const tit_len = 30;
 
 //API 설정
 const api_key = 'AIzaSyBzu-DiHzM7s-z6s-Elyy24zf2bBrf6ckk';
@@ -7,6 +8,9 @@ const pid = 'PL2pfG9YtKxMJVwlAjAIV9TBF5-44WnQ-q';
 const num = 5;
 const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
+//일정 글자수 이살일때 글자 자르고 말줄임표 붙이기
+//문자열.substr(시작위치,자를글자수)
+
 fetch(resultURL)
 	.then((data) => data.json())
 	.then((json) => {
@@ -14,12 +18,23 @@ fetch(resultURL)
 		let tags = '';
 
 		json.items.map((data) => {
+			let desc = data.snippet.description;
+			desc.length > 120 ? (desc = desc.substr(0, 120) + '...') : desc;
+
+			//날짜값 가공
+			let date = data.snippet.publishedAt.split('T')[0];
+			date = date.split('-').join('.');
+
 			tags += `
       <article>
-        <h2>${data.snippet.title}</h2>
+        <h2>${
+					data.snippet.title.length > tit_len
+						? data.snippet.title.substr(0, tit_len) + '...'
+						: data.snippet.title
+				}</h2>
         <div class='txt'>
-          <p>${data.snippet.description}</p>
-          <span>${data.snippet.publishedAt}</span>
+          <p>${desc}</p>
+          <span>${date}</span>
         </div>
         <div class='pic'>
           <img src='${data.snippet.thumbnails.standard.url}'
